0fa50c972eff0192d5bd6a282d2794d1a776f223
MoroccoBS <chouaibwidadipur@gmail.com>
2023-10-18 16:45:39 +0100

    Updated Pall func

--- operations1.c
+++ operations1.c
@@ -33,6 +33,8 @@ void push(stack_t **stack, unsigned int line_number)
 
 void pall(stack_t **stack, unsigned int line_number)
 {
+	/** (void)(stack);
+	printf("%d\n", line_number);
 	stack_t *current = *stack;
 
 	(void)(line_number);
@@ -41,6 +43,18 @@ void pall(stack_t **stack, unsigned int line_number)
 	{
 		printf("%d\n", current->n);
 		current = current->next;
+	}
+	*/
+	stack_t *tmp;
+
+	(void)line_number;
+	if (stack == NULL)
+		exit(EXIT_FAILURE);
+	tmp = *stack;
+	while (tmp != NULL)
+	{
+		printf("%d\n", tmp->n);
+		tmp = tmp->next;
 	}
 }
 
