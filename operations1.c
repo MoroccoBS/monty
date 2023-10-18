9cb1b6e5fd95e42d987512e4ce7dbabc8b176429
MoroccoBS <chouaibwidadipur@gmail.com>
2023-10-18 01:11:33 +0100

    Added mandatory tasks

---
+++ operations1.c
@@ -0,0 +1,142 @@
+#include "monty.h"
+
+/**
+ * push - pushes an element to the stack
+ * @stack: double pointer to the first node
+ * @line_number: line number of the operation
+ * Return: void
+ */
+
+void push(stack_t **stack, unsigned int line_number)
+{
+	stack_t *new_node = malloc(sizeof(stack_t));
+	char *operation;
+	int value;
+	char *endptr;
+
+	if (stack == NULL)
+	{
+		printf("L%u: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	if (new_node == NULL)
+	{
+		printf("Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+
+	operation = strtok(NULL, " \t\n");
+	if (operation == NULL)
+	{
+		printf("L%u: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	value = strtol(operation, &endptr, 10);
+	if (*endptr != '\0')
+	{
+		printf("L%u: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	new_node->n = value;
+	new_node->prev = NULL;
+	new_node->next = *stack;
+
+	if (new_node->next != NULL)
+	{
+		(new_node->next)->prev = new_node;
+	}
+	if (*stack != NULL)
+	{
+		(*stack)->prev = new_node;
+	}
+
+	*stack = new_node;
+}
+
+/**
+ * pall - prints all values on the stack from the top
+ * @stack: double pointer to the first node
+ * @line_number: line number of the operation
+ * Return: void
+ */
+
+void pall(stack_t **stack, unsigned int line_number)
+{
+	stack_t *current = *stack;
+
+	(void)(line_number);
+
+	while (current != NULL)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+	}
+}
+
+/**
+ * pint - prints the value at the top of the stack
+ * @stack: double pointer to the first node
+ * @line_number: line number of the operation
+ * Return: void
+ */
+
+void pint(stack_t **stack, unsigned int line_number)
+{
+	if (stack == NULL || *stack == NULL)
+	{
+		printf("L%u: can't pint if stack is empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*stack)->n);
+}
+
+/**
+ * pop - removes top element of stack
+ * @stack: double pointer to the first node
+ * @line_number: line number of the operation
+ * Return: void
+ */
+
+void pop(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp = *stack;
+
+	if (stack == NULL || *stack == NULL)
+	{
+		printf("L%u: can't pop if stack is empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	*stack = (*stack)->next;
+
+	if (*stack != NULL)
+	{
+		(*stack)->prev = NULL;
+	}
+
+	free(temp);
+}
+
+/**
+ * swap - swaps the top two elements of the stack
+ * @stack: double pointer to the first node
+ * @line_number: line number of the operation
+ * Return: void
+ */
+
+void swap(stack_t **stack, unsigned int line_number)
+{
+	int temp = (*stack)->n;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		printf("L%u: can't swap stack has less than 2 elements\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	(*stack)->n = (*stack)->next->n;
+	(*stack)->next->n = temp;
+}
---
+++ operations2.c
@@ -0,0 +1,104 @@
+#include "monty.h"
+
+/**
+ * add - adds top 2 nodes of the stack
+ * @stack: double pointer to the first node
+ * @line_number: line number of the operation
+ * Return: void
+ */
+
+void add(stack_t **stack, unsigned int line_number)
+{
+	int sum = (*stack)->n + (*stack)->next->n;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		printf("L%u: can't add stack has less than 2 elements\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	pop(stack, line_number);
+	(*stack)->n = sum;
+}
+
+/**
+ * nop - does anything
+ * @stack: double pointer to the first node
+ * @line_number: line number of the operation
+ * Return: void
+ */
+
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
+
+/**
+ * sub - subtracts the top element of the stack from the second element
+ * @stack: double pointer to the first node
+ * @line_number: line number of the operation
+ * Return: void
+ */
+
+void sub(stack_t **stack, unsigned int line_number)
+{
+	int difference = (*stack)->next->n - (*stack)->n;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		printf("L%u: can't sub stack has less than 2 elements\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	pop(stack, line_number);
+	(*stack)->n = difference;
+}
+
+/**
+ * div - divides the second element of the stack by the top element.
+ * @stack: double pointer to the first node
+ * @line_number: line number of the operation
+ * Return: void
+ */
+
+void divide(stack_t **stack, unsigned int line_number)
+{
+	int quotient = (*stack)->next->n / (*stack)->n;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		printf("L%u: can't div stack has less than 2 elements\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	if ((*stack)->n == 0)
+	{
+		printf("L%u: can't divide by zero\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	pop(stack, line_number);
+	(*stack)->n = quotient;
+}
+
+/**
+ * mul - multiplies the second element of the stack by the top element.
+ * @stack: double pointer to the first node
+ * @line_number: line number of the operation
+ * Return: void
+ */
+
+void mul(stack_t **stack, unsigned int line_number)
+{
+	int product = (*stack)->next->n * (*stack)->n;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		printf("L%u: can't mul stack has less than 2 elements\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	pop(stack, line_number);
+	(*stack)->n = product;
+}
