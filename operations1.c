d35828761fbcbc657822fd0da5e22401ba1017f4
MoroccoBS <chouaibwidadipur@gnail.com>
2023-10-19 23:50:01 +0100

    Fixed swap

--- operations1.c
+++ operations1.c
@@ -105,15 +105,21 @@ void pop(stack_t **stack, unsigned int line_number)
 
 void swap(stack_t **stack, unsigned int line_number)
 {
-	int temp = (*stack)->n;
+	int temp;
 
-	if (*stack == NULL || (*stack)->next == NULL)
+	if (*stack == NULL)
 	{
-		fprintf(
-			stderr, "L%u: can't swap, stack too short\n", line_number);
-		exit(EXIT_FAILURE);
+		fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
+		return;
 	}
 
+	if ((*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
+		return;
+	}
+
+	temp = (*stack)->n;
 	(*stack)->n = (*stack)->next->n;
 	(*stack)->next->n = temp;
 }
